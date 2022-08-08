9debdeb3c1d670c425f475ae0d6e4d115e42d366
Kaushan <kaushandutta5@gmail.com>
2022-08-08 23:49:45 +0530

    updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import SanityClient from "../client";
 
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
-}
+  const [getProject, setProject] = useState([]);
 
-export default Project
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const data = await SanityClient.fetch('*[_type=="project"]');
+
+      if (data) {
+        console.log(data);
+        setProject(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <main>
+      <section>
+        <h2>My Projects</h2>
+        <h2>Welcome to my Project Page</h2>
+        <section>
+          {getProject.map((object)=>{return(
+            <article key={object.title}>
+            <h3>
+              <a>
+                {object.description}
+              </a>
+            </h3>
+            <div>
+              <span>
+                {object.title}
+              </span>
+              <span></span>
+              <p></p>
+              <span></span>
+            </div>
+          </article>
+          )})}
+          
+
+
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Project;
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import SanityClient from "../client";
+import imageUrlBuilder from "@sanity/image-url";
 
 const SinglePost = () => {
-  return (
-    <div>SinglePost</div>
-  )
-}
+  useEffect(() => {
+    getData();
+  }, []);
 
-export default SinglePost
+  const [singlePost, setPost] = useState([]);
+
+  const { slug } = useParams();
+  //console.log(slug);
+
+  const builder = imageUrlBuilder(SanityClient);
+
+  function urlFor(source) {
+    return builder.image(source);
+  }
+
+  const getData = async () => {
+    try {
+      const responce = await SanityClient.fetch(
+        '*[slug.current=="' + slug + '"]'
+      );
+
+      console.log(responce);
+
+      if (responce) {
+        setPost(responce[0]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div>SinglePost</div>
+      <h1>{singlePost.title}</h1>
+      <img
+                      src={urlFor(singlePost.mainImage.asset._ref).width(200).url()}
+                     
+                    />
+    </>
+  );
+};
+
+export default SinglePost;
